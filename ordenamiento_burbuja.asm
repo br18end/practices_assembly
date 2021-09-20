bb8645bd11d4c4fc46bc46faf8a0d7103ef573e7
br18end <43587217+br18end@users.noreply.github.com>
2021-09-19 17:37:50 -0700

    Update ordenamiento_burbuja.asm

--- ordenamiento_burbuja.asm
+++ ordenamiento_burbuja.asm
@@ -1,73 +1,51 @@
-TITLE MASM Template                        (main.asm)
-
-
+TITLE MASM Template (main.asm)
 ; Description:
-; 
+;
 ; Revision date:
-
-
 INCLUDE Irvine32.inc
 .data
 arreglo dword 100 dup (?)
-espacio byte " ",0
-tamarreglo dword ?
+tamArreglo dword ?
 intercambios dword ?
-
-
+muestra byte "Los valores del arreglo son: ",0
+espacio byte " ",0
 .code
 main PROC
-    lea ebx,arreglo
-    call Randomize
-    mov ecx,100
-
-
-ciclo:
-    call Random32
-    mov [ebx],eax
-    add ebx,4
-    call writedec
-    lea edx,espacio
-    call writestring
-    loop ciclo
-
-
-    mov tamarreglo,lengthof arreglo
-
-
+mov edx, offset muestra
+call writeString
+mov ebx,offset arreglo
+mov ecx,lengthof arreglo
+call Randomize
+L1: call Random32
+mov edx, offset espacio
+call writeString
+call writeDec
+mov [ebx],eax
+loop L1
+mov tamArreglo,lengthof arreglo
 inicio:
-    mov intercambios,0
-    dec tamarreglo
-    mov ecx,tamarreglo
-    xor ebx,ebx
-
-
+dec tamArreglo
+mov ecx,tamArreglo
+xor ebx,ebx
+mov intercambios, ebx
 ciclo1:
-    mov eax,arreglo[ebx]
-    cmp eax,arreglo[ebx+4]
-    xchg eax,[ebx+4]
-    xchg [ebx],eax
-    xchg esi,[ebx]
-    xchg [ebx+4],esi
-    dec ecx
-cmp ecx,0
-    jg ciclo1
-
-
-    cmp intercambios,0
-    jne inicio
-
-
-;ciclo:
-;    mov [ebx],eax
-;    add ebx,4
-;    call writedec
-;    lea edx,espacio
-;    call writestring
-;    loop ciclo
-
-
-    exit
+mov eax,arreglo[ebx]
+cmp eax,arreglo[ebx+4]
+jge sigue
+xchg eax,arreglo[ebx+4]
+xchg eax, arreglo[ebx]
+sigue:
+loop ciclo1
+cmp intercambios,0
+Jne inicio
+mov edx, offset muestra
+call writeString
+mov ecx,lengthof arreglo
+L2:
+mov edx, offset espacio
+call writeString
+call writeDec
+loop L2
+exit
 main ENDP
-
-
 END main
